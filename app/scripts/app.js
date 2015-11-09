ce0f503860c04a27c80172107a4c060d35d7d0ea
Erkki Heino <heinoe2@tml.hut.fi>
2015-11-09 22:12:02 +0200

    "Language support"

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -22,61 +22,62 @@ angular
     'truncate'
   ])
   .config(function($routeProvider) {
+      var lang = '/:lang?';
     $routeProvider
-      .when('/page', {
-        templateUrl: 'views/semantic_page.html',
-        controller: 'SemanticPageCtrl',
-        controllerAs: 'ctrl'
-      })
-      .when('/events/page', {
+      .when(lang + '/events/page', {
         templateUrl: 'views/page.html',
         controller: 'PageCtrl',
         controllerAs: 'ctrl'
       })
-      .when('/events/:era?', {
+      .when(lang + '/events/:era?', {
         templateUrl: 'views/simile_map.html',
         controller: 'SimileMapCtrl',
         controllerAs: 'timemapCtrl',
         reloadOnSearch: false
       })
-      .when('/units/page', {
+      .when(lang + '/units/page', {
         templateUrl: 'views/unit_page.html',
         controller: 'UnitPageCtrl',
         controllerAs: 'ctrl'
       })
-      .when('/persons/page', {
+      .when(lang + '/persons/page', {
         templateUrl: 'views/person_page.html',
         controller: 'PersonPageCtrl',
         controllerAs: 'ctrl'
       })
-      .when('/ranks/page', {
+      .when(lang + '/ranks/page', {
         templateUrl: 'views/rank_page.html',
         controller: 'RankPageCtrl',
         controllerAs: 'ctrl'
       })
-      .when('/units/', {
+      .when(lang + '/units/', {
         templateUrl: 'views/actor_timeline.html',
         controller: 'ActorCtrl',
         controllerAs: 'timemapCtrl',
         reloadOnSearch: false
       })
-      .when('/persons/', {
+      .when(lang + '/persons/', {
         templateUrl: 'views/person_timeline.html',
         controller: 'PersonDemoCtrl',
         controllerAs: 'ctrl',
          reloadOnSearch: false
       })
-      .when('/times/page', {
+      .when(lang + '/times/page', {
         templateUrl: 'views/time_page.html',
         controller: 'TimePageCtrl',
         controllerAs: 'ctrl'
       })
-      .when('/photographs/page', {
+      .when(lang + '/photographs/page', {
         templateUrl: 'views/semantic_page.html',
         controller: 'SemanticPageCtrl',
         controllerAs: 'ctrl'
       })
-      .when('/casualties/page', {
+      .when(lang + '/casualties/page', {
+        templateUrl: 'views/semantic_page.html',
+        controller: 'SemanticPageCtrl',
+        controllerAs: 'ctrl'
+      })
+      .when(lang + '/page', {
         templateUrl: 'views/semantic_page.html',
         controller: 'SemanticPageCtrl',
         controllerAs: 'ctrl'
