f11c835dca46662c6db4f2c729eca10554ededf6
Erkki Heino <heinoe2@tml.hut.fi>
2015-12-08 15:32:18 +0200

    Add comments to sparql_service.js

--- app/scripts/common/sparql_service.js
+++ app/scripts/common/sparql_service.js
@@ -3,15 +3,22 @@
 angular.module('eventsApp')
 /*
  * Service for providing SPARQL query paging.
+ * sparqlQry is the SPARQL query for the whole data that should be paged.
+ * pageSize is the size of one page.
+ * getResults is the function that is used for querying for results.
  */
 .factory('PagerService', function($q) {
     return function(sparqlQry, pageSize, getResults) {
+
         var countify = function(sparqlQry) {
+            // Form a query that counts the total number of items returned
+            // by the query.
             return sparqlQry.replace(/\bselect\b.+?(where)?\W+?\{/i,
                 'SELECT (COUNT(DISTINCT ?id) AS ?count) WHERE {');
         };
 
         var pagify = function(sparqlQry, page, pageSize) {
+            // Form the query for the given page.
             return sparqlQry + ' LIMIT ' + pageSize + ' OFFSET ' + ((page - 1) * pageSize);
         };
 
@@ -19,10 +26,15 @@ angular.module('eventsApp')
         var count;
 
         this.getTotalCount = function() {
+            // Get the total number of items that the original query returns.
+            // Returns a promise.
+
+            // Get cached count if available.
             if (count) {
                 return $q.when(count);
             }
             return getResults(countQry, true).then(function(results) {
+                // Cache the count.
                 count = parseInt(results[0].count.value);
                 return count;
             });
@@ -56,6 +68,12 @@ angular.module('eventsApp')
         };
     };
 })
+/* Service for querying a SPARQL endpoint with paging support.
+ * Takes the endpoint URL and a mapper object as parameters.
+ * The mapper is an object that maps the SPARQL results to objects.
+ * The mapper should provide a 'makeObjectList' function that takes the SPARQL
+ * results as parameter and returns the mapped objects.
+ * */
 .factory('AdvancedSparqlService', function($http, $q, SparqlService, PagerService) {
     return function(endpointUrl, mapper) {
         var endpoint = new SparqlService(endpointUrl);
@@ -72,7 +90,7 @@ angular.module('eventsApp')
 
         return {
             getObjects: function(sparqlQry, pageSize) {
-                // If pageSize is defined, return (a promise of) a PagerService object, otherwise
+                // If pageSize is defined, return a PagerService object, otherwise
                 // query the endpoint and return the results as a promise.
                 if (pageSize) {
                     return new PagerService(sparqlQry, pageSize, getResults);
