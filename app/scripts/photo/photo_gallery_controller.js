2afdb9eb79860522a78f9205c70897793e5164fe
Erkki Heino <heinoe2@tml.hut.fi>
2015-12-22 15:12:49 +0200

    Photo gallery in event page; refactor

--- app/scripts/photo/photo_gallery_controller.js
+++ app/scripts/photo/photo_gallery_controller.js
@@ -8,7 +8,7 @@
  * Controller of the eventsApp
  */
 angular.module('eventsApp')
-.controller('PhotoGalleryCtrl', function($scope, $timeout, $log, $window) {
+.controller('PhotoGalleryCtrl', function($scope, $q, $timeout, $log, $window) {
     var self = this;
 
     self.isCollapsed = true;
@@ -16,24 +16,15 @@ angular.module('eventsApp')
     self.isLoadingImages;
     self.photos = [];
 
-    self.toggleCollapse = function() {
-        if (self.imageCount !== self.photos.length) {
-            getAllPhotos().then(function() {
-                self.isCollapsed = !self.isCollapsed;
-            });
-        } else {
-            self.isCollapsed = !self.isCollapsed;
-        }
-    };
+    self.toggleCollapse = toggleCollapse;
 
     var win = angular.element($window);
 
-    win.bind('resize', checkOverFlow);
+    win.bind('resize', checkOverflow);
 
     $scope.$on('$destroy', function() {
-        win.unbind('resize', checkOverFlow);
+        win.unbind('resize', checkOverflow);
     });
-
 
     $scope.$watch('images', function(val) {
         self.imageCount = 0;
@@ -44,21 +35,35 @@ angular.module('eventsApp')
         self.isLoadingImages = true;
         self.imagePager.getTotalCount().then(function(count) {
             self.imageCount = count;
+            if (!count) {
+                // No images to fetch, stop here.
+                return $q.reject();
+            }
         })
         .then(function() { return self.imagePager.getPage(0); })
         .then(function(page) {
             self.photos = page;
             $timeout(function() {
-                checkOverFlow();
+                checkOverflow();
             }, 0);
             self.isLoadingImages = false;
         }).catch(function() {
-            $log.error('Error while fetching photos.');
             self.isLoadingImages = false;
         });
     });
 
-    function checkOverFlow() {
+    function toggleCollapse() {
+        if (self.imageCount !== self.photos.length) {
+            getAllPhotos().then(function() {
+                self.isCollapsed = !self.isCollapsed;
+            });
+        } else {
+            self.isCollapsed = !self.isCollapsed;
+        }
+    }
+
+
+    function checkOverflow() {
         if (self.photos == false) {
             self.isLoadingImages = false;
             return;
