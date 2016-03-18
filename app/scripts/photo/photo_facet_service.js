e99173dfb5ad267267ad242412ca0eace11bcebd
Erkki Heino <heinoe2@tml.hut.fi>
2016-03-18 12:16:11 +0200

    Make changes to reflect changes in the faceted search package; modify photo demo help text

--- app/scripts/photo/photo_facet_service.js
+++ app/scripts/photo/photo_facet_service.js
@@ -12,7 +12,7 @@
 
     /* @ngInject */
     function photoFacetService($q, $translate, _, photoRepository,
-            FacetSelectionFormatter, SPARQL_ENDPOINT_URL, PHOTO_PAGE_SIZE) {
+            facetSelectionFormatter, SPARQL_ENDPOINT_URL, PHOTO_PAGE_SIZE) {
 
         var facets = {
             '<http://ldf.fi/kuvanottoaika>' : {
@@ -21,18 +21,25 @@
                 start: '<http://purl.org/dc/terms/created>',
                 end: '<http://purl.org/dc/terms/created>',
                 min: '1939-10-01',
-                max: '1945-12-31'
+                max: '1945-12-31',
+                enabled: true
             },
-            '<http://purl.org/dc/terms/description>': { name: 'DESCRIPTION', type: 'text' },
+            '<http://purl.org/dc/terms/description>': {
+                name: 'DESCRIPTION',
+                type: 'text',
+                enabled: true
+            },
             '<http://purl.org/dc/terms/spatial>': {
                 name: 'PLACE',
-                service: '<http://ldf.fi/pnr/sparql>'
+                service: '<http://ldf.fi/pnr/sparql>',
+                enabled: true
             },
-            '<http://purl.org/dc/terms/subject>': { name: 'PERSON' },
+            '<http://purl.org/dc/terms/subject>': {
+                name: 'PERSON',
+                enabled: true
+            },
             '<http://purl.org/dc/terms/creator>': { name: 'PHOTOGRAPHER' }
         };
-
-        var formatter = new FacetSelectionFormatter(facets);
 
         var facetOptions = {
             endpointUrl: SPARQL_ENDPOINT_URL,
@@ -48,7 +55,7 @@
         this.getFacetOptions = getFacetOptions;
 
         function getResults(facetSelections) {
-            var selections = formatter.parseFacetSelections(facetSelections);
+            var selections = facetSelectionFormatter.parseFacetSelections(facets, facetSelections);
             return photoRepository.getByFacetSelections(selections, fetchOptions);
         }
 
