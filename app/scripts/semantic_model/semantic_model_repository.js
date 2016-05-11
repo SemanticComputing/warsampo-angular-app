f5cb2337a8660b7d3a69cde0966e8b9f7ccc7926
Erkki Heino <heinoe2@tml.hut.fi>
2016-05-11 19:31:32 +0300

    Arbitrary resource page redone
    - Pagination for related links
    - Refactored

--- app/scripts/semantic_model/semantic_model_repository.js
+++ app/scripts/semantic_model/semantic_model_repository.js
@@ -1,65 +1,89 @@
-'use strict';
+(function() {
+    'use strict';
+    /* eslint-disable angular/no-service-method */
 
-/*
- * Service that provides an interface for fetching semanticModels from the WarSa SPARQL endpoint.
- */
-angular.module('eventsApp')
-    .service('semanticModelRepository', function($q, SparqlService,
-                semanticModelMapperService) {
+    /*
+    * Service that provides an interface for fetching arbitrary resources
+    * from the WarSa SPARQL endpoint.
+    */
+    angular.module('eventsApp')
+    .service('semanticModelRepository', semanticModelRepository);
 
-        var endpoint = new SparqlService('http://ldf.fi/warsa/sparql');
+    function semanticModelRepository($q, AdvancedSparqlService, semanticModelMapperService,
+            objectMapperService, QueryBuilderService) {
 
-        var prefixes = '' +
-            ' PREFIX skos: <http://www.w3.org/2004/02/skos/core#> ' +
-            ' PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> ' +
-            ' PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> ';
+        /* Public API */
+
+        this.getById = getById;
+        this.getRelated = getRelated;
+
+        /* Implementation */
+
+        var endpoint = new AdvancedSparqlService('http://ldf.fi/warsa/sparql',
+                semanticModelMapperService);
+
+        var relatedEndpoint = new AdvancedSparqlService('http://ldf.fi/warsa/sparql',
+                objectMapperService);
+
+        var prefixes =
+        ' PREFIX skos: <http://www.w3.org/2004/02/skos/core#> ' +
+        ' PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> ' +
+        ' PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> ';
+
+        var queryBuilder = new QueryBuilderService(prefixes);
 
         var byIdQry = prefixes +
-            ' SELECT DISTINCT ?id  ?label ?type ?type_label ?pred ?pred_label ?obj ?obj_label ' +
-            ' WHERE { ' +
-            '   VALUES ?id { {0} }  ' +
-            '   ?id ?pred ?obj . ' +
-            '   ?id a ?type . ' +
-            '   OPTIONAL { ?type rdfs:label|skos:prefLabel|skos:altLabel ?type_label . } ' +
-            '   OPTIONAL { ?id rdfs:label|skos:prefLabel|skos:altLabel ?label . } ' +
-            '   OPTIONAL { ?pred rdfs:label|skos:prefLabel|skos:altLabel ?pred_label . } ' +
-            '   OPTIONAL { ?obj rdfs:label|skos:prefLabel|skos:altLabel ?obj_label . } ' +
-            ' } ';
+        ' SELECT DISTINCT ?id  ?label ?link_label ?type ?type_label ?pred ?pred_label ?obj ' +
+        '  ?obj_label ?link { ' +
+        '  VALUES ?id { {0} }  ' +
+        '  ?id ?pred ?obj . ' +
+        '  ?id a ?type . ' +
+        '  { ' +
+        '   SELECT DISTINCT ?link { ' +
+        '    VALUES ?id { {0} }  ' +
+        '    ?o ?link ?id .' +
+        '   } ' +
+        '  } ' +
+        '  OPTIONAL { ' +
+        '   FILTER(BOUND(?link)) ' +
+        '   ?link skos:prefLabel ?link_label . ' +
+        '  } ' +
+        '  OPTIONAL { ?type rdfs:label|skos:prefLabel|skos:altLabel ?type_label . } ' +
+        '  OPTIONAL { ?id rdfs:label|skos:prefLabel|skos:altLabel ?label . } ' +
+        '  OPTIONAL { ?pred rdfs:label|skos:prefLabel|skos:altLabel ?pred_label . } ' +
+        '  OPTIONAL { ?obj rdfs:label|skos:prefLabel|skos:altLabel ?obj_label . } ' +
+        ' } ';
+
+        var relatedQryResultSet =
+        ' VALUES ?ref { {0} } ' +
+        ' VALUES ?pred { {1} } ' +
+        ' ?id ?pred ?ref . ';
 
         var relatedQry = prefixes +
-            ' SELECT DISTINCT ?id ?obj ?obj_label  ' +
-            '       ?obj_type (COALESCE(?filabel, ?deflabel) AS ?label) ' +
-            '       ?type (COALESCE(?fi_pred_label, ?def_pred_label) AS ?pred_label) ?pred ' +
-            ' WHERE { ' +
-            '   VALUES ?ref { {0} }  ' +
-            '   ?obj ?id ?ref . ' +
-            '   ?obj a ?pred . ' +
-            '   BIND(rdf:Property AS ?type) ' +
-            '   OPTIONAL { ' +
-            '       ?id rdfs:label|skos:prefLabel|skos:altLabel ?filabel . ' +
-            '       FILTER(langmatches(lang(?filabel), "FI")) } ' +
-            '   OPTIONAL { ?id rdfs:label|skos:prefLabel|skos:altLabel ?deflabel . } ' +
-            '   OPTIONAL { ?obj rdfs:label|skos:prefLabel|skos:altLabel ?obj_label . } ' +
-            '   OPTIONAL { ?pred rdfs:label|skos:prefLabel|skos:altLabel ?fi_pred_label .  ' +
-            '       FILTER(langmatches(lang(?fi_pred_label), "FI")) } ' +
-            '   OPTIONAL { ?pred rdfs:label|skos:prefLabel|skos:altLabel ?def_pred_label . } ' +
-            ' } ';
+        ' SELECT DISTINCT ?id ?label { ' +
+        '  <RESULT_SET> ' +
+        '  OPTIONAL { ' +
+        '   ?id rdfs:label|skos:prefLabel|skos:altLabel ?lbl . ' +
+        '   FILTER(langmatches(lang(?lbl), "FI")) } ' +
+        '  OPTIONAL { ?id rdfs:label|skos:prefLabel|skos:altLabel ?lbl . } ' +
+        '  BIND(COALESCE(?lbl, REPLACE(STR(?id), "^.+[/#](.+?)$", "$1")) as ?label)' +
+        ' } ';
 
-        this.getById = function(id) {
+        function getById(id) {
             var qry = byIdQry.format('<' + id + '>');
             return endpoint.getObjects(qry).then(function(data) {
                 if (data.length) {
-                    return semanticModelMapperService.makeObjectList(data)[0];
+                    return data[0];
                 }
-                return $q.reject("Failed to get SemanticModel");
+                return $q.reject('Failed to retrieve object with id ' + id);
             });
-        };
+        }
 
-        this.getRelated = function(id) {
-            var qry = relatedQry.format('<' + id + '>');
-            return endpoint.getObjects(qry).then(function(data) {
-                return semanticModelMapperService.makeObjectList(data);
-            });
-        };
-});
+        function getRelated(id, link) {
+            var resultSet = relatedQryResultSet.format('<' + id + '>', '<' + link + '>');
+            var qryObj = queryBuilder.buildQuery(relatedQry, resultSet);
+            return relatedEndpoint.getObjects(qryObj.query, 10, qryObj.resultSetQuery);
+        }
+    }
+})();
 
