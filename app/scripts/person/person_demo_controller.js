926a1c43d67eea878cdac66e1534ef0b646c5e9d
Erkki Heino <evsheino@gmail.com>
2016-08-24 12:32:53 +0300

    Clean up person demo controller

--- app/scripts/person/person_demo_controller.js
+++ app/scripts/person/person_demo_controller.js
@@ -1,89 +1,78 @@
-'use strict';
+(function() {
+    'use strict';
 
-/**
- * @ngdoc function
- * @name eventsApp.controller:PersonDemoCtrl
- * @description
- * # PersonDemoCtrl
- * Controller of the eventsApp
- */
-angular.module('eventsApp')
-.controller('PersonDemoCtrl', function($route, $routeParams, $location, $q, $scope,
-              $rootScope, eventService, personService) {
+    angular.module('eventsApp')
+    .controller('PersonDemoCtrl', PersonDemoController);
 
-    $rootScope.showSettings = null;
-    $rootScope.showHelp = null;
+    /* @ngInject */
+    function PersonDemoController($routeParams, $location, personService) {
+        var self = this;
 
-    var self = this;
+        self.items = [];
+        self.queryregex = '';
 
-    self.updateByUri = function(uri) {
-        self.isLoadingEvent = true;
-        self.isLoadingLinks = false;
-        personService.getById(uri)
-    .then(function(person) {
-        self.person = person;
-        self.isLoadingEvent = false;
+        self.updateByUri = updateByUri;
+        self.updateActor = updateActor;
+        self.getItems = getItems;
 
-        return personService.fetchRelated2(person);
-    }).catch(function() {
-        self.isLoadingEvent = false;
-        self.isLoadingLinks = false;
-    });
-    };
+        init();
 
-    self.items = [];
-    self.queryregex = '';
+        function init() {
+            self.getItems();
 
-    self.getItems = function () {
-        var rx='', n=self.queryregex.length;
-        if (n<1) {
-            rx= '^AA.*$';
-        } else if (n<2) {
-            rx= '^'+self.queryregex+'A.*$';
-        } else {
-            rx= self.queryregex;
-            if (rx.indexOf(' ')>0) {
-                var arr=rx.split(' ');
-                rx='';
-                for (var i=0; i<arr.length; i++) {
-                    rx += '(?=.*'+arr[i]+')';
+            if ($routeParams.uri) {
+                self.updateByUri($routeParams.uri);
+            } else {
+                self.selectedItem = { name: 'Talvela, Paavo Juho', id: 'http://ldf.fi/warsa/actors/person_50' };
+                self.updateActor();
+            }
+        }
+
+        function updateByUri(uri) {
+            self.isLoadingEvent = true;
+            self.isLoadingLinks = false;
+            personService.getById(uri)
+            .then(function(person) {
+                self.person = person;
+                self.isLoadingEvent = false;
+
+                return personService.fetchRelated2(person);
+            }).catch(function() {
+                self.isLoadingEvent = false;
+                self.isLoadingLinks = false;
+            });
+        }
+
+        function getItems() {
+            var rx='', n=self.queryregex.length;
+            if (n<1) {
+                rx= '^AA.*$';
+            } else if (n<2) {
+                rx= '^'+self.queryregex+'A.*$';
+            } else {
+                rx= self.queryregex;
+                if (rx.indexOf(' ')>0) {
+                    var arr=rx.split(' ');
+                    rx='';
+                    for (var i=0; i<arr.length; i++) {
+                        rx += '(?=.*'+arr[i]+')';
+                    }
                 }
+                rx= '(^|^.* )'+rx+'.*$';
             }
-            rx= '(^|^.* )'+rx+'.*$';
+            personService.getItems(rx,self);
         }
-        personService.getItems(rx,self);
-    };
 
-    self.getItems();
+        function updateActor() {
+            if (self.selectedItem && self.selectedItem.id) {
+                var uri = self.selectedItem.id;
 
-    self.updateActor = function () {
-        if (self.selectedItem && self.selectedItem.id) {
-            var uri=self.selectedItem.id;
+                if ($location.search().uri != uri) {
+                    $location.search('uri', uri);
+                }
 
-            if ($location.search().uri != uri) {
-                self.noReload = true;
-                $location.search('uri', uri);
+                self.updateByUri(uri);
             }
-
-            self.updateByUri(uri);
         }
-    };
-
-    // Set listener to prevent reload when it is not desired.
-    $scope.$on('$routeUpdate', function() {
-        if (!self.noReload) {
-            $route.reload();
-        } else {
-            self.noReload = false;
-        }
-    });
-
-    if ($routeParams.uri) {
-        self.updateByUri($routeParams.uri);
-    } else {
-        self.selectedItem = { name: 'Talvela, Paavo Juho', id: 'http://ldf.fi/warsa/actors/person_50' };
-        self.updateActor();
     }
-});
-
-
+})();
