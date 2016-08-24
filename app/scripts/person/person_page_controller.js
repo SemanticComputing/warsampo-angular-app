756107baf90b9f2668e1c5fdc2bf831ca61e242a
Erkki Heino <evsheino@gmail.com>
2016-08-24 12:36:54 +0300

    Clean up person page controller

--- app/scripts/person/person_page_controller.js
+++ app/scripts/person/person_page_controller.js
@@ -1,31 +1,26 @@
-'use strict';
+(function() {
+    'use strict';
 
-/**
- * @ngdoc function
- * @name eventsApp.controller:UnitPageCtrl
- * @description
- * # UnitPageCtrl
- * Controller of the eventsApp
- */
-angular.module('eventsApp')
-.controller('PersonPageCtrl', function($routeParams, $q, $rootScope, eventService, personService) {
-    $rootScope.showSettings = null;
-    $rootScope.showHelp = null;
-    var self = this;
-    if ($routeParams.uri) {
-        self.isLoadingEvent = true;
-        self.isLoadingLinks = false;
-        personService.getById($routeParams.uri)
-        .then(function(person) {
-            self.person = person;
-            self.isLoadingEvent = false;
+    angular.module('eventsApp')
+    .controller('PersonPageCtrl', PersonPageController);
 
-            return personService.fetchRelated(person);
-        }).catch(function() {
-            self.isLoadingEvent = false;
+    /* @ngInject */
+    function PersonPageController($routeParams, $q, $rootScope, eventService, personService) {
+        var self = this;
+
+        if ($routeParams.uri) {
+            self.isLoadingEvent = true;
             self.isLoadingLinks = false;
-        });
+            personService.getById($routeParams.uri)
+            .then(function(person) {
+                self.person = person;
+                self.isLoadingEvent = false;
+
+                return personService.fetchRelated(person);
+            }).catch(function() {
+                self.isLoadingEvent = false;
+                self.isLoadingLinks = false;
+            });
+        }
     }
-});
-
-
+})();
