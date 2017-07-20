003a1b0c1027a25071adca125773c2dfb5d6e625
Esko Ikkala <esko.ikkala@aalto.fi>
2017-07-20 15:21:21 +0300

    Fix cemetery photo gallery layout

--- app/scripts/common/directives/ws-fallback-img-for-gallery.directive.js
+++ app/scripts/common/directives/ws-fallback-img-for-gallery.directive.js
@@ -8,8 +8,13 @@
             link: function(scope, element) {
                 element.error(function() {
                     var src = '/images/no-image-sm.png';
+                    var src_big = '/images/no-image.png';
                     if (!_.includes(element.attr('src'), src)) {
-                        element.attr('src', src);
+                        if (element[0].className == 'simple-gallery-thumbnail') {
+                            element.attr('src', src_big);
+                        } else {
+                            element.attr('src', src);
+                        }
                         var a = element.parent();
                         a.attr('href', '/images/no-image.png');
                         a.attr('target', '_self');
