1a54dc54dfee6e430c92d63828b16d024167868e
Erkki Heino <heinoe2@tml.hut.fi>
2017-09-20 15:23:53 +0300

    Improve how prisoner data is displayed

--- app/scripts/person/person_page_controller.js
+++ app/scripts/person/person_page_controller.js
@@ -5,31 +5,43 @@
     .controller('PersonPageController', PersonPageController);
 
     /* @ngInject */
-    function PersonPageController($log, $location, $routeParams, personService) {
+    function PersonPageController($log, $location, $routeParams, _, personService) {
         var self = this;
 
-        if ($location.host() === 'www.sotasampo.fi') {
-            self.showPrisonerRecord = false;
-        } else {
-            self.showPrisonerRecord = true;
+        self.showHr = showHr;
+
+        init();
+
+        var hrProps = ['other_information', 'memoirs'];
+
+        function init() {
+            if ($location.host() === 'www.sotasampo.fi') {
+                self.showPrisonerRecord = false;
+            } else {
+                self.showPrisonerRecord = true;
+            }
+
+            if ($routeParams.uri) {
+                self.isLoadingPerson = true;
+                self.isLoadingRelated = true;
+                personService.getById($routeParams.uri)
+                .then(function(person) {
+                    self.person = person;
+                    self.isLoadingPerson = false;
+                    return personService.fetchRelated(person);
+                })
+                .then(function() {
+                    self.isLoadingRelated = false;
+                }).catch(function(err) {
+                    $log.error(err);
+                    self.isLoadingPerson = false;
+                    self.isLoadingRelated = false;
+                });
+            }
         }
 
-        if ($routeParams.uri) {
-            self.isLoadingPerson = true;
-            self.isLoadingRelated = true;
-            personService.getById($routeParams.uri)
-            .then(function(person) {
-                self.person = person;
-                self.isLoadingPerson = false;
-                return personService.fetchRelated(person);
-            })
-            .then(function() {
-                self.isLoadingRelated = false;
-            }).catch(function(err) {
-                $log.error(err);
-                self.isLoadingPerson = false;
-                self.isLoadingRelated = false;
-            });
+        function showHr(prop) {
+            return _.includes(hrProps, prop);
         }
     }
 })();
